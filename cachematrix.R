b7ed0cc77f1dc1494d9ba381e82e1d5e32d03d6c
BoheeLEE <65781138+BoheeLEE@users.noreply.github.com>
2020-05-23 00:51:58 +0100

    week3 programming assignment
    R programming

--- cachematrix.R
+++ cachematrix.R
@@ -5,17 +5,17 @@ library(matlib)
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-  mat.inv <- NULL
-  set.mat <- function(y) {
-    x <<- y
-    mat.inv <<- NULL
+makeCacheMatrix <- function(x = matrix()) {  # define a function with a default mode of 'matrix'
+  mat.inv <- NULL                            # set mat.inv as Null, not specified a default value, this will hold the matrix inverse
+  set.mat <- function(y) {                   # define a function as set.mat as new
+    x <<- y                                  # the operator <<- is only used in Functions, and cause a search to be made through parent environments for an existing definition of the variable being assigned 
+    mat.inv <<- NULL                         
   }
-  get.mat <- function() x
-  setinverse <- function(Inverse) mat.inv <<- Inverse
-  getinverse <- function() mat.inv
-  list(set.mat = set.mat, get.mat = get.mat,
-       setinverse = setinverse,
+  get.mat <- function() x                    # define the get.mat function which returns the matrix argument
+  setinverse <- function(Inverse) mat.inv <<- Inverse  # set the value of inverse in parent environment
+  getinverse <- function() mat.inv                     # get the value of inverse 
+  list(set.mat = set.mat, get.mat = get.mat,          
+       setinverse = setinverse(matrix()),
        getinverse = getinverse)
 }
 
@@ -23,15 +23,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  mat.inv <- x$getinverse()
-  if(!is.null(mat.inv)) {
-    message("getting cached data")
-    return(mat.inv)
+  mat.inv <- x$getinverse()                 
+  if(!is.null(mat.inv)) {                     # if the inverse matrix is not null then you will get the message "getting cached data"
+    message("getting cached data")           
+    return(mat.inv)                           # return the inverse matrix - exit 
   }
-  data <- x$get.mat()
-  mat.inv <- solve(data, ...)
-  x$setinverse(mat.inv)
-  mat.inv
+  data <- x$get.mat()                         # if the inverse matrix is null then get the original inverse matrix
+  mat.inv <- solve(data, ...)                 # solve(): to compute the inverse matrix 
+  x$setinverse(mat.inv)                       # set the inverse matrix
+  mat.inv                                     # print
 }
 
 
